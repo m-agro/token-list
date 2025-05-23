78306ff7774db994ca4e96f11819195b0614dcd0
m-agro <arvyduosius@gmail.com>
2025-05-23 23:14:02 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,4 @@
-export * from './lib/tokenlist';
+git checkout -b add-magro-token
+git add src/tokens/solana/2zhKM3FzEnLhbvUaaGiKQRnCi4EoWogJcG365CxF733Q.json
+git commit -m "Add MAGRO Token (2zhKM3FzEnLhbvUaaGiKQRnCi4EoWogJcG365CxF733Q)"
+git push origin add-magro-token
